23e07ee8c8930ca06b1af356f80c85047f5f29b0
lebrancconvas <lebrancconvas@gmail.com>
2023-09-10 20:02:44 +0700

    ✨ feat: add text to title screen

--- src/scenes/TitleScene.ts
+++ src/scenes/TitleScene.ts
@@ -11,7 +11,8 @@ export default class TitleScene extends Phaser.Scene {
 
   create() {
     this.add.image(400, 300, 'bg');
-    this.add.text(window.innerWidth / 2, window.innerHeight / 2 - 200, 'Hello Nature', { color: '#0f0', fontSize: '100px' }).setOrigin(0.5, 0.5);
+    this.add.text(window.innerWidth / 2, window.innerHeight / 2 - 200, 'Hello Nature', { color: '#0f0', fontSize: '100px', stroke: '#00f', strokeThickness: 20 }).setOrigin(0.5, 0.5);
+    this.add.text(window.innerWidth / 2, window.innerHeight / 2 + 200, 'Press Space to Start Game.', { color: '#0f0', fontSize: '80px', stroke: '#00f', strokeThickness: 20 }).setOrigin(0.5, 0.5);
   }
 
   update() {
