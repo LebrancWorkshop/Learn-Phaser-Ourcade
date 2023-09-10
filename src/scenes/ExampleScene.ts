aca916d94d4192c45d0fbb2bd755f4bd3b8b29cc
lebrancconvas <lebrancconvas@gmail.com>
2023-09-10 18:10:26 +0700

    :tada: Init.

---
+++ src/scenes/ExampleScene.ts
@@ -0,0 +1,19 @@
+import { Phaser } from '../phaser';
+
+export default class ExampleScene extends Phaser.Scene {
+  constructor() {
+    super('scene');
+  }
+
+  preload() {
+
+  }
+
+  create() {
+
+  }
+
+  update() {
+    
+  }
+};
---
+++ src/scenes/index.ts
@@ -0,0 +1,5 @@
+import ExampleScene from './ExampleScene';
+
+export const scenes = [
+  ExampleScene
+];
